52e0be989445635d4319df13b8cd5e2b6e3e529f
Nick Lee <nickoless.lee@gmail.com>
2023-03-29 16:46:46 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import { useEffect } from 'react';
+import Gallery from 'Gallery';
+
+const App = () => {
+    return (
+        <div className="App">
+            <Gallery />
+        </div>
+    );
+};
+
+export default App;
